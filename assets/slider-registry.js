6f042ff348c26ac7413d0db01734fe4b1edb4fce
Aimen Tetbirt <tetbirta@yahoo.com>
2026-06-20 12:20:25 +0100

    nos coffrets

--- assets/slider-registry.js
+++ assets/slider-registry.js
@@ -2,13 +2,18 @@ export const sliderRegistry = {
   scrolling_text: {
     type: "carousel",
     perView: 3,
-    autoplay: 1,
-    animationDuration: 5000,
+    focusAt: "center",
+    autoplay: 16,
+    animationDuration: 3000,
     rewind: false,
     animationTimingFunc: "cubic-bezier(0,0,1,1)",
     breakpoints: {
       768: { perView: 2 },
       480: { perView: 1.2 },
     },
+  },
+  products: {
+    type: "slider",
+    perView: 3,
   },
 };
